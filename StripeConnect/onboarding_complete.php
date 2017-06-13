5d32a193649e361c6e03d0432ea13a3663a83e4c
RoadeoCowboy <RoadeoCowboy@gmail.com>
2017-06-13 15:54:35 -0700

    Added Stripe Connect redirect URI.

---
+++ StripeConnect/onboarding_complete.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<title>Stripe Connect Complete</title>
+<head>
+<link rel="stylesheet" href="css/stripe-connect.css">
+</head>
+
+<body>
+	<p>Stripe Account Creation Complete!</p>
+
+	<script type="text/javascript">
+
+	</script> 
+</body>
+
+</html>
